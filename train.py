fda4357d140870e454f9b6de7a71a4e21cc050cd
Pedro <almeida.pedro.ivo@gmail.com>
2021-03-02 11:55:21 -0300

    update to train file

--- train.py
+++ train.py
@@ -6,8 +6,11 @@ def train():
 
 	G6.iris_model_train(train_database_path,train_encoding_model_path)
 
-if __name__ == '__main__':
+def test(filename = None):
 	test_encoding_model_path = r'encodingModel/irisEncodings.pickle'
 	name = G6.iris_model_test(test_encoding_model_path,r'Input_Database/Juliano/Juliano_1.jpg')
 
 	print(f'Name is {name}')
+
+if __name__ == '__main__':
+	test()
